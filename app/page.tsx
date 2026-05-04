7729fe07873236a16cf4cef21340953934712717
greentreerecord-hue <greentreerecord@gmail.com>
2026-05-04 12:50:27 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,19 @@ export default function Page() {
     <div style={{ padding: "20px", background: "black", color: "white" }}>
       <h1>Ray'sStream 🔥</h1>
 
-      <h2>Scenic Album</h2>
+      {/* Video */}
+      <h2>It's Cool</h2>
       <video
         src="/videos/itscool.mp4"
         controls
         autoPlay
         loop
         muted
-        style={{ width: "100%", borderRadius: "12px" }}
+        style={{
+          width: "100%",
+          borderRadius: "12px",
+          marginBottom: "20px",
+        }}
       />
     </div>
   );
