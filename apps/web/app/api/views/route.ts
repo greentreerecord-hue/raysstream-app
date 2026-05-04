1c4fb684a4da984bb93cc96e54c5233677142442
greentreerecord-hue <greentreerecord@gmail.com>
2026-05-04 14:42:43 -0700

    Update route.ts

--- apps/web/app/api/views/route.ts
+++ apps/web/app/api/views/route.ts
@@ -1,34 +1,11 @@
-export default function Home() {
-  return (
-    <main
-      style={{
-        padding: 20,
-        background: "#111",
-        color: "white",
-        minHeight: "100vh",
-      }}
-    >
-      <h1>Ray&apos;sStream 🔥</h1>
-      <p>It&apos;s Cool</p>
+import { NextResponse } from "next/server";
 
-      <video
-        src="/videos/test.mp4"
-        controls
-        muted
-        autoPlay
-        style={{
-          width: "100%",
-          maxWidth: "650px",
-          borderRadius: "12px",
-          background: "black",
-        }}
-      />
+let views = 0;
 
-      <h2 style={{ marginTop: 10 }}>Spaceship</h2>
+export async function GET() {
+  views = views + 1;
 
-      <p style={{ color: "#aaa", marginTop: 5 }}>
-        1 view
-      </p>
-    </main>
-  )
+  return NextResponse.json({
+    views: views,
+  });
 } 
