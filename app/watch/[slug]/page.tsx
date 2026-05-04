c7780da4415685c8465789566273f0d791e660a5
greentreerecord-hue <greentreerecord@gmail.com>
2026-05-03 22:15:24 -0700

    Update page.tsx

--- app/watch/[slug]/page.tsx
+++ app/watch/[slug]/page.tsx
@@ -1,24 +1,8 @@
-export default function HomePage() {
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>Ray'sStream</h1>
-
-      <p>Welcome to Ray'sStream</p>
-
-      <a
-        href="/watch/test"
-        style={{
-          display: "inline-block",
-          marginTop: "20px",
-          padding: "12px 18px",
-          background: "black",
-          color: "white",
-          borderRadius: "10px",
-          textDecoration: "none"
-        }}
-      >
-        Watch Spaceship Video
-      </a>
-    </div>
-  )
-} 
+<video
+  controls
+  autoPlay
+  loop
+  className="w-full rounded-lg"
+>
+  <source src="/videos/spaceship.mp4" type="video/mp4" />
+</video> 
