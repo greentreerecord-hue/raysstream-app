4459c540fb435cac29ab1b03b26f882093472b15
greentreerecord-hue <greentreerecord@gmail.com>
2026-05-04 14:30:13 -0700

    Update route.ts

--- apps/web/app/api/views/route.ts
+++ apps/web/app/api/views/route.ts
@@ -1,1 +1,20 @@
+let views: Record<string, number> = {}
 
+export async function POST(req: Request) {
+  const { id } = await req.json()
+
+  if (!views[id]) views[id] = 0
+
+  views[id] += 1
+
+  return Response.json({ views: views[id] })
+}
+
+export async function GET(req: Request) {
+  const url = new URL(req.url)
+  const id = url.searchParams.get("id") || "test"
+
+  return Response.json({
+    views: views[id] || 0,
+  })
+}
