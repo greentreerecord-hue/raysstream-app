c738e18f0122ca9e99f18ef48071e4d443cb89e3
greentreerecord-hue <greentreerecord@gmail.com>
2026-05-05 14:45:10 -0700

    Update page.tsx

--- apps/web/app/watch/[slug]/page.tsx
+++ apps/web/app/watch/[slug]/page.tsx
@@ -1,16 +1,7 @@
 export default function WatchPage() {
   return (
-    <div>
-      <h1>Watching: test</h1>
-
-      <video
-        width="800"
-        controls
-        autoPlay
-        muted
-      >
-        <source src="/videos/its-cool.mp4" type="video/mp4" />
-      </video>
-    </div>
+    <video width="900" controls autoPlay muted>
+      <source src="/videos/its-cool.mp4" type="video/mp4" />
+    </video>
   );
 } 
