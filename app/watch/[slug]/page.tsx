7e3cafbe3ea8e32201a15f88964cff5222ca23b5
greentreerecord-hue <greentreerecord@gmail.com>
2026-05-05 14:32:07 -0700

    Update page.tsx

--- app/watch/[slug]/page.tsx
+++ app/watch/[slug]/page.tsx
@@ -1,11 +1,21 @@
-export default function WatchPage({ params }: { params: { slug: string } }) {
+export default function WatchPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
   return (
-    <div>
+    <div style={{ padding: "20px" }}>
       <h1>Watching: {params.slug}</h1>
 
-      <video width="600" controls autoPlay>
-        <source src="/videos/its-cool.mp4" type="video/mp4" />
+      <video
+        width="800"
+        controls
+        autoPlay
+        style={{ marginTop: "20px" }}
+      >
+        <source src="/videos/Its-cool.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
       </video>
     </div>
   );
-} 
+}
