87d56795d66560562cf3074f259c97426a4c5c3b
greentreerecord-hue <greentreerecord@gmail.com>
2026-05-04 18:44:19 -0700

    Update page.tsx

--- app/watch/[slug]/page.tsx
+++ app/watch/[slug]/page.tsx
@@ -1,7 +1,25 @@
-export default function Page({ params }: { params: { slug: string } }) {
+import { kv } from "@vercel/kv";
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const key = `video:${params.slug}:views`;
+
+  // get current views
+  let views = await kv.get<number>(key);
+
+  if (!views) {
+    views = 0;
+  }
+
+  // increase views
+  await kv.incr(key);
+
   return (
     <main style={{ padding: "24px" }}>
       <h1>Watching: {params.slug}</h1>
+
+      <p style={{ fontSize: "20px", marginBottom: "12px" }}>
+        👁️ {views + 1} views
+      </p>
 
       <video
         controls
